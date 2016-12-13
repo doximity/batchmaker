40f433d88b28aa8d74990f9f73d1e26df2e60e5a
Krish Rajagopalan <krajagopalan@doximity.com>
2016-12-13 12:57:01 -0800

    Extract batcher class from Doximity for email queuing

---
+++ lib/batcher.rb
@@ -0,0 +1,129 @@
+class Batcher
+  VERSION = '0.1.0'
+  StoppedError = Class.new(StandardError)
+
+  def initialize(name, size, tick_period, logger, notifier = nil, &block)
+    @name     = name
+    @size     = size
+    @queue    = Queue.new
+    @mutex    = Mutex.new
+    @count    = 0
+    @action   = block
+    @logger   = Rails.logger if defined?(Rails)
+    @notifier = notifier
+    @stopping  = false
+
+    # Main thread that process the batches
+    @thread = Thread.new(&method(:run))
+
+    # Thread that ticks on a specified frequency
+    @tick = Thread.new(tick_period, &method(:tick))
+    @tick.abort_on_exception = true
+    @tick.priority = 1
+  end
+
+  def running?
+    ["run", "sleep"].include?(@thread.status) && !@stopping
+  end
+
+  def <<(item)
+    if !running?
+      raise StoppedError, "failure to queue item, #{ident_str} has already stopped"
+    end
+
+    @mutex.synchronize do
+      @queue << [:add, item]
+      @count += 1
+
+      if @count >= @size
+        @count = 0
+        @queue << [:process, :size]
+      end
+    end
+
+    Thread.pass
+  end
+
+  def shutdown!
+    shutdown && wait
+  end
+
+  def shutdown
+    info "shutting down"
+
+    @mutex.synchronize do
+      @stopping = true
+      @queue << [:process]
+      @queue << [:stop]
+    end
+
+    @tick.exit
+  end
+
+  def wait
+    @thread.join
+  end
+
+  private
+
+  def run
+    batch = []
+
+    info "starting batch processing loop"
+    loop do
+      action, *args = @queue.pop
+
+      case action
+      when :add
+        batch << args.first
+        info "item added to #{@name} batch: #{args.first.inspect}"
+
+      when :process
+        unless batch.empty?
+          begin
+            @action.(batch.freeze)
+            info "batch processed with #{batch.size} items"
+          rescue => e
+            error "batch with #{batch.size} failed to process"
+            debug "batch content when failed: #{batch.inspect}"
+            @notifier.log_and_notify(e, batcher_id: ident_str) unless @notifier.nil?
+            next
+          ensure
+            batch = []
+          end
+        end
+
+      when :stop
+        break
+      end
+    end
+
+    info "exiting batch processing loop"
+  end
+
+  def tick(period)
+    loop do
+      sleep(period)
+
+      @mutex.synchronize do
+        @queue << [:process, :tick]
+      end
+    end
+  end
+
+  def ident_str
+    "[#{self.class.name}(#{@name}):#{Process.pid}]"
+  end
+
+  def error(msg)
+    @logger.error "#{ident_str} error: #{msg}"
+  end
+
+  def info(msg)
+    @logger.info "#{ident_str} info: #{msg}"
+  end
+
+  def debug(msg)
+    @logger.debug "#{ident_str} debug: #{msg}"
+  end
+end
---
+++ spec/batcher_spec.rb
@@ -0,0 +1,76 @@
+require "spec_helper"
+
+RSpec.describe Batcher do
+  let(:queue) { Queue.new }
+  let(:logger) { double(:logger) }
+
+  let :action do
+    -> (batch) { queue << batch }
+  end
+
+  before do
+    allow(logger).to receive(:info)
+    allow(logger).to receive(:error)
+    allow(logger).to receive(:debug)
+  end
+
+  it "call actions with batch of defined sizes" do
+    batcher = described_class.new("test", 10, 1, logger, &action)
+    20.times.each { |i| batcher << i  }
+    batcher.shutdown!
+
+    batches = consume(queue)
+    expect(batches).to eq [Array(0..9), Array(10..19)]
+  end
+
+  it "calls actions respecting max size when ticking" do
+    batcher = described_class.new("test", 10, 0.05, logger, &action)
+
+    10.times.each do |i|
+      batcher << i
+      sleep(0.1) if i == 7
+    end
+
+    batcher.shutdown!
+    batches = consume(queue)
+    expect(batches).to eq([Array(0..7), Array(8..9)])
+  end
+
+  it "handles errors properly" do
+    already_failed = false
+    batcher = described_class.new("test", 5, 1, logger,) do |batch|
+      will_fail, already_failed = !already_failed, true
+      raise "some error" if will_fail
+      queue << batch
+    end
+
+    10.times.each { |i| batcher << i }
+    batcher.shutdown!
+
+    batches = consume(queue)
+    expect(batches).to eq([Array(5..9)])
+  end
+
+  it "calls action with remaining items when shutting down" do
+    batcher = described_class.new("test", 10, 10, logger, &action)
+    batcher << 1
+    batcher.shutdown!
+
+    batches = consume(queue)
+    expect(batches).to eq([[1]])
+  end
+
+  it "fails to add items after shutdown" do
+    batcher = described_class.new("test", 10, 10, logger, &action)
+    batcher.shutdown!
+    expect { batcher << 1 }.to raise_error(Batcher::StoppedError)
+  end
+
+  private
+
+  def consume(queue)
+    consumed = []
+    consumed << queue.pop(true) while queue.size > 0
+    consumed
+  end
+end
